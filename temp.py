1eae062d7d459bf09f7190b4dbf9de123d1f6617
Ubin108 <ybin0108@gmail.com>
2025-03-23 16:43:27 +0000

    edit videos

--- temp.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-# Load the .tif image
-tif_image_path = '/hdd_1/ubin108/MetaFormer/assets/images/real/input/139.tif'
-image = Image.open(tif_image_path)
-
-# Save the image as .png
-png_image_path = '/hdd_1/ubin108/MetaFormer/assets/images/real/input/139.png'
-image.save(png_image_path, 'PNG')
